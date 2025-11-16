13a6573e700e6e6bc39567d73fb90fc60664fbc3
Enzo-Aita <Enzoaitanaime99@gmail.com>
2025-11-16 14:30:34 -0300

    refactor
    # Conflicts:
    #	.gitignore
    #	LICENSE
    #	README.md

--- src/main/java/modelo/Categoria.java
+++ src/main/java/modelo/Categoria.java
@@ -1,26 +1,23 @@
 package modelo;
 
-import dao.CategoriaDAO;
-import java.util.ArrayList;
-
+import java.io.Serializable;
 
 /**
- * Representa uma categoria
- * 
- * <p>
- * contém as informações: Nome, embalagem e tamanho
- * Encapsula operações de CRUD por um {@link CategoriaDAO}
- * </p>
+ * Categoria de produto Contém os atributos para identificação: nome, embalagem
+ * e tamanho
  */
-public class Categoria {
+public class Categoria implements Serializable {
+
+    private static final long serialVersionUID = 1L;
 
     private int id;
     private String nome;
     private String embalagem;
     private String tamanho;
-    private CategoriaDAO dao;
 
-    /** Construtor padrão */
+    /**
+     * Construtor padrão
+     */
     public Categoria() {
         this(0, "", "", "");
     }
@@ -28,150 +25,92 @@ public class Categoria {
     /**
      * Construtor completo
      *
-     * @param id ID da categoria
+     * @param id Código de identificação
      * @param nome Nome da categoria
-     * @param embalagem Tipo de embalagem da categoria
-     * @param tamanho Tamanho da categoria
+     * @param embalagem Tipo de embalagem
+     * @param tamanho Tamanho da embalagem
      */
     public Categoria(int id, String nome, String embalagem, String tamanho) {
         this.id = id;
         this.nome = nome;
         this.embalagem = embalagem;
         this.tamanho = tamanho;
-        this.dao = new CategoriaDAO();
     }
 
     /**
-     * Construtor com só ID e nome
+     * Construtor Básico para categoria
      *
-     * @param idCategoria ID da categoria
+     * @param idCategoria Código identificação da categoria
      * @param nomeCategoria Nome da categoria
      */
-    public Categoria(int idCategoria, String nomeCategoria){
+    public Categoria(int idCategoria, String nomeCategoria) {
         this.id = idCategoria;
         this.nome = nomeCategoria;
     }
-    
-    /** @return Retorna o ID da categoria */
+
+    /**
+     * @return Retorna o ID da categoria
+     */
     public int getId() {
         return id;
     }
 
-    /** @param id Seta um novo ID para a categoria */
+    /**
+     * @param id Seta o ID da categoria
+     */
     public void setId(int id) {
         this.id = id;
     }
 
-    /** @return Retorna o nome da categoria */
+    /**
+     * @return Retorna o Nome da categoria
+     */
     public String getNome() {
         return nome;
     }
 
-    /** @param nome Seta um novo nome para a categoria */
+    /**
+     * @param nome Seta o nome da categoria
+     */
     public void setNome(String nome) {
         this.nome = nome;
     }
 
-    /** @return Retorna o tipo de embalagem da categoria */
+    /**
+     * @return Retorna o tipo de embalagem
+     */
     public String getEmbalagem() {
         return embalagem;
     }
 
-    /** @param embalagem Seta um novo tipo de embalagem para a categoria */
+    /**
+     * @param embalagem Seta o tipo de embalagem
+     */
     public void setEmbalagem(String embalagem) {
         this.embalagem = embalagem;
     }
 
-    /** @return Retorna o tamanho da categoria */
+    /**
+     * @return Retorna o tamanho da embalagem
+     */
     public String getTamanho() {
         return tamanho;
     }
 
-    /** @param tamanho Seta um novo tamanho para a categoria */
+    /**
+     * @param tamanho Seta o tamanho da embalagem
+     */
     public void setTamanho(String tamanho) {
         this.tamanho = tamanho;
     }
 
-    /** @return Retorna o Dao utilizado pela classe */
-    public CategoriaDAO getDao() {
-        return dao;
-    }
-
-    /**
-     * Seta um novo Dao para operar com o BD
-     * @param dao Instância de {@link CategoriaDAO}
-     */
-    public void setDao(CategoriaDAO dao) {
-        this.dao = dao;
-    }
-
-
-    /**
-     * Retorna a lista de categorias cadastradas
-     *
-     * @return lista de objetos {@link Categoria}
-     */
-    public ArrayList<Categoria> getMinhaLista() {
-        return dao.getMinhaLista();
-    }
-
-    /**
-     * Insere uma nova categoria no BD
-     *
-     * @param nome Nome da categoria
-     * @param embalagem Tipo de embalagem
-     * @param tamanho Tamanho da categoria
-     * @return Retorna true caso a operação seja bem-sucedida
-     */
-    public boolean insertCategoriaBD(String nome, String embalagem, String tamanho) {
-        int id = this.maiorID() + 1;
-        Categoria objeto = new Categoria(id, nome, embalagem, tamanho);
-        dao.insertCategoriaBD(objeto);
-        return true;
-    }
-
-    /**
-     * Deleta uma categoria do BD
-     *
-     * @param id ID da categoria a ser excluída
-     * @return Retorna true caso a categoria seja excluída
-     */
-    public boolean deleteCategoriaBD(int id) {
-        dao.deleteCategoriaBD(id);
-        return true;
-    }
-
-/**
- * Atualiza uma categoria cadastrada
- *
- * @param id ID da categoria
- * @param nome Novo nome para a categoria
- * @param embalagem Nova embalagem para a categoria
- * @param tamanho Novo tamanho para a categoria
- * @return Retorna true caso a atualização seja bem-sucedida
- */    
-public boolean updateCategoriaBD(int id, String nome, String embalagem, String tamanho) {
-        Categoria objeto = new Categoria(id, nome, embalagem, tamanho);
-        return dao.updateCategoriaBD(objeto);
-    }
-    
-    /**
-     * Carrega uma categoria do BD
-     *
-     * @param id ID da categoria
-     * @return Retorna o objeto {@link Categoria} correspondente
-     */
-    public Categoria carregaCategoria(int id) {
-        return dao.listarCategoria(id);
-    }
-
-
-    /**
-     * Obtém o maior ID no BD
-     *
-     * @return maior ID encontrado
-     */
-    public int maiorID() {
-        return dao.maiorID();
+    @Override
+    public String toString() {
+        return "Categoria{"
+                + "id=" + id
+                + ", nome='" + nome + '\''
+                + ", embalgem='" + embalagem + '\''
+                + ",tamanho='" + tamanho + '\''
+                + '}';
     }
 }
